6e6a13b20c6727403df098fc414fa9b964f10f05
Ganesh Shankar Chavan <ganeshchavan8491@gmail.com>
2025-05-21 18:57:35 +0530

    Add files via upload

---
+++ termuxpkg.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+clear
+echo "-----------------------------------------------------------------------------"
+echo " This Script is written by GANESH CHAVAN"
+echo "-----------------------------------------------------------------------------"
+
+
+
+echo ""
+echo " If You Connected To Internet Then your setup completed successfully."
+echo " Follow on Instagram: @ganeshchavan_gc"
+echo " Follow on Github: @ganuchavan8491"
+echo " Subscribe YouTube Channel: @Me_BEROJAGAR_ENGINEER
+"
+
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo "#####################################"
+echo " We are Learners"               
+echo " NOT BAD HACKERS "                                                                    
+echo "#####################################"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo "[+] Updating packages..."
+apt-get update -y && apt-get upgrade -y
+
+echo "[+] Installing Programming Languages..."
+apt install python -y
+apt install python2 -y
+apt install ruby -y 
+apt install php -y 
+apt install perl -y
+
+echo "[+] Installing Developer Tools..."
+apt install git -y
+apt install clang -y
+apt install bash -y
+apt install jq -y
+apt install nano -y
+apt install curl -y
+apt install wget -y
+apt install zip -y
+apt install unzip -y
+apt install tar -y
+apt install sudo -y
+
+echo "[+] Installing Code Editors..."
+apt install vim -y
+apt install neovim -y
+pkg install code-oss -y    # For Termux
+
+echo "[+] Installing Terminal Beautification..."
+apt install screenfetch -y
+apt install neofetch -y
+apt install toilet -y
+apt install figlet -y
+apt install cowsay -y
+apt install fish -y
+apt install cmatrix -y
+
+echo "[+] Installing Networking Tools..."
+apt install nmap -y
+pkg install openssh -y
+apt install w3m -y
+apt install tor -y
+apt install net-tools -y
+apt install macchanger -y
+apt install openssl -y
+apt install bmon -y
+apt install wcalc -y
+
+echo "[+] Installing Utilities..."
+apt install tree -y
+apt install tmux -y
+apt install htop -y
+
+echo "#####################################"
+echo " u can now hack anything      "               
+echo " completing wait 3 2 1 "                                                                    
+echo "#####################################"
+
+echo "##########GANESH#CHAVAN###########"
+echo "SpyD3r"               
+echo "#######GANESH#SpyD3r##############"
+
+echo "########SpyD3r#GANESH############"
+echo " processing wait......"
+echo "##########GANESH#CHAVAN#############"
+
+echo ""
+echo " If You Connected To Internet Then your setup completed successfully."
+echo " Follow on Instagram: @ganeshchavan_gc"
+echo " Follow on Github: @ganuchavan8491"
+echo " Subscribe YouTube Channel: @Me_BEROJAGAR_ENGINEER
+"
+echo " Don't Learn To Hack, Hack To Learn"
+
+exit
